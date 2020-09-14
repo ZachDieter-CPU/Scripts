d2829a02ebcf54767151e50a95c84fc95774c6bf
Donkeysniper <58150743+Donkeysniper@users.noreply.github.com>
2020-09-14 15:54:41 -0400

    Add files via upload
    Get a manager from a list of users

---
+++ GetManagerPowerShell.ps1
@@ -0,0 +1,29 @@
+﻿$List = Get-Content "C:\Users\zdieter\Documents\Scripts\GetManager\EnabledList.txt"
+Connect-AzureAD
+
+
+ForEach($Item in $List){
+    $info=@{}
+    
+    $userDetails = Get-ADUser -Filter 'Name -like $Item' -properties displayName, manager
+    
+    $info["User"]=$userDetails.displayName
+  
+    IF ($userDetails.manager) # if not null
+    
+    {
+    
+    $managerDetails = Get-ADUser (Get-ADUser -Filter 'Name -like $Item' -properties manager).manager -properties displayName
+    
+    $info["Manager"]=$managerDetails.displayName
+    
+    }
+    
+    New-Object -TypeName PSObject -Property $info
+        
+}
+
+
+
+
+#$UserWriter | Out-File -FilePath "C:\Users\zdieter\Documents\Scripts\GetManager\UserWithManger.txt"
